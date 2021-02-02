50c962796a29929ac4bf680d1a8cc9793e1e4cd3
Jay Miles <jay.miles@addenbrookes.nhs.uk>
2021-02-02 13:40:55 +0000

    added two more unit tests

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -26,6 +26,15 @@ class DatabaseTests(unittest.TestCase):
         """Test that the total number of itmems returns the correct value."""
         self.assertEquals(self.db_mod.get_total_number_items(), 8218165)
 
+    def get_avg_ACTCOST(self):
+        """Test that the average ACT cost is correct."""
+        self.assertEquals(self.db_mod.get_avg_ACTCOST(), 76.22)
+
+    def get_top_prescribed_item(self):
+        """Test that the top prescribed item is identified correctly."""
+        self.assertEquals(self.db_mod.get_top_prescribed_item(), (869879, 
+        'Methadone HCl_Oral Soln 1mg/1ml S/F', 0.14))
+
     def test_get_unique_drugs(self):
         """Test that the total number of unique drugs returns the correct value."""
         self.assertEquals(self.db_mod.get_unique_drugs(), 13922)
